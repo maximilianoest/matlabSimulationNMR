16c72977f627de13ed3db11e83c44d1cea45c8f9
maximilianoest <50007913+maximilianoest@users.noreply.github.com>
2022-09-26 15:09:59 +0200

    calculate, plot and save R1 for different lipids

--- evaluation/LipidWaters_deltaTAndNN/evaluationMain.m
+++ evaluation/LipidWaters_deltaTAndNN/evaluationMain.m
@@ -1,36 +1,76 @@
 clc; clear all; close all;
 
+%% load results and determine R1
 addpath(genpath('../../library'));
+addpath(genpath('../../txtFiles'));
+constants = readConstantsFile('constants.txt');
 resultsPath = '../../RESULTS/findDeltaTAndNN_lipidWater_longSimulationTime';
+savingPath = sprintf('%s%sPlots%s',resultsPath,filesep,filesep);
 addpath(genpath(resultsPath));
+fieldStrengthInT = 3; % main magnetic field strength
+omega0 = constants.gyromagneticRatioOfHydrogenAtom * fieldStrengthInT;
+dipolDipolConstant = 3/4*(constants.vaccumPermeability/(4*pi) ...
+    *constants.hbar*constants.gyromagneticRatioOfHydrogenAtom^2)^2 ...
+    /(constants.nanoMeter^6);
 
 allMatFilesInResultsDirectory = dir( ...
     sprintf('%s%s*.mat',resultsPath,filesep));
 
-dataForEvaluation = struct();
+relevantResults = struct();
 
 for fileCounter = 1:size(allMatFilesInResultsDirectory,1)
-fileName = allMatFilesInResultsDirectory(fileCounter,1).name;
-if strcmp(fileName,"")
-    error("Matfile name not found");
-end
-results = load(sprintf('%s%s%s',resultsPath,filesep,fileName));
-simulationParameters = struct( ...
-    'deltaT', results.deltaTInS ...
-    ,'simulationDuration', results.simulationDurationInS ...
-    ,'sumCorrFuncZerothOrder', results.sumCorrelationFunctionsSaverZerothOrder ...
-    ,'sumCorrFuncFirstOrder', results.sumCorrelationFunctionSaverFirstOrder ...
-    ,'sumCorrFuncSecondOrder', results.sumCorrelationFunctionSaverSecondOrder);
-
-whichLipid = results.whichLipid;
-if ~isfield(dataForEvaluation,whichLipid)
-    dataForEvaluation.(whichLipid) = {};
-end
-dataForEvaluation.(whichLipid){end+1} = simulationParameters; 
-
+    fileName = allMatFilesInResultsDirectory(fileCounter,1).name;
+    
+    if strcmp(fileName,"")
+        error("Matfile name not found. ");
+    end
+    
+    filePath = sprintf('%s%s%s',resultsPath,filesep,fileName);
+    load(filePath,'whichLipid');
+    if ~isfield(relevantResults,whichLipid)
+        relevantResults.(whichLipid) = {};
+    end
+    relevantResults.(whichLipid){end+1} = loadResultsAndDetermineR1( ...
+        filePath,omega0,dipolDipolConstant);
+    relevantResults.(whichLipid){end}.('fieldStrengthInT') = fieldStrengthInT;
     
 end
 
+clearvars -except relevantResults savingPath
+%% Present/Plot results
+
+% 1. plot r1 rates
+colors = getColorsFromColorConfigurationFile();
+for whichLipid = fieldnames(relevantResults)'
+    lipidData = relevantResults.(whichLipid{1});
+    initializeFigure();
+    legendEntries = {};
+    for datasetNr = 1:size(lipidData,2)
+        dataset = lipidData{datasetNr};
+        r1_theta_phi = dataset.r1_theta_phi;
+        title(sprintf('Lipid: %s', whichLipid{1}));
+        for thetaNr = 1:size(dataset.fibreAnglesTheta,2)
+            for phiNr = 1:size(dataset.fibreAnglesPhi,2)
+                r1_NN = [];
+                for nearestNeighbourCase = fieldnames(dataset.r1_theta_phi)'
+                    r1_NN(end+1) = r1_theta_phi.(nearestNeighbourCase{1})(thetaNr,phiNr); %#ok<SAGROW>
+                end
+                plot(dataset.nearestNeighbourCases,r1_NN);
+                legendEntries{end+1} = sprintf('simDuration: %.2d, dt: %.2d, $\\theta$: %.2f, $\\phi$: %.2f', dataset.simulationDuration, dataset.deltaT, dataset.fibreAnglesTheta(thetaNr), dataset.fibreAnglesPhi(phiNr)); %#ok<SAGROW>
+            end
+        end
+    end
+    legend(legendEntries);
+    savingName = sprintf('%srelaxationRates%s',savingPath,filesep); 
+    fileSavingName = sprintf('%s%s_R1ForDifferentSimConfigs',savingName,whichLipid{1});
+    if ~exist(savingName,'dir')
+        mkdir(savingName)
+    end
+    print(gcf,fileSavingName,'-dpng','-r300');
+end
+
+
+% 2. plot the correlation functions 
 
 
 
---
+++ library/plotting/getColorsFromColorConfigurationFile.m
@@ -0,0 +1,16 @@
+function [colors] = getColorsFromColorConfigurationFile()
+
+colorConfiguration = readConfigurationFile('colors.txt');
+configColors = colorConfiguration.colors;
+configColors = split(configColors,";");
+colors = [];
+
+explicitFTColor = '#6699ff';
+explicitFTColor = sscanf(explicitFTColor(2:end),'%2x%2x%2x',[1 3])/255;
+
+for colorNr = 1:length(configColors)
+    color = configColors{colorNr};
+    colors(end+1,1:3) = sscanf(color(2:end),'%2x%2x%2x',[1 3])/255;
+end
+
+end
