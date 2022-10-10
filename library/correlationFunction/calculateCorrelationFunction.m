220688e2f8dd82b5760c863d89302ead479b6200
maximilianoest <50007913+maximilianoest@users.noreply.github.com>
2022-10-10 10:59:06 +0200

    Fixed correlation function and some formatting things

--- library/correlationFunction/calculateCorrelationFunction.m
+++ library/correlationFunction/calculateCorrelationFunction.m
@@ -1,18 +1,16 @@
 function [sumCorrelationFunction] = ...
-    calculateCorrelationFunction(sphericalHarmonic,numLags)
+    calculateCorrelationFunction(sphericalHarmonic)
 
 % EXPLANATIONS OF CALCULATION:
-% IFFt is slower than FFT, because FFT has a build in function which
-% detects whether the input data is real or complex and therefore chooses
-% an optimized way to calculate the inverse FT. Because the input data of
-% IFFT is real, this data can be seen as conjugate symmetric. Therefore,
-% the 'symmetric' flag at IFFT is set.
+% The symmetric flag for ifft can't be set here because the imaginary part
+% of the correlation function is necessary! Therefore, the return value of
+% this function is a complex correlation function.
 % The reason why in frequency domain the squared real and imaginary part
 % are summed is, because the autocorrelation require the "convolution" (not
 % directly the convolution, but more the correlation) of a signal and its 
 % complex conjugate. This is a multiplication of the signal
 % and its complex conjugate in frequency domain and therefore the real and
-% imaginary part summed up.
+% imaginary part is squared and then added up.
 %
 % fft creates a vector with higher amplitudes => to get a valid result from
 % fft you have to devide by the length of the vector
@@ -20,11 +18,12 @@ function [sumCorrelationFunction] = ...
 % ifft(fft(x)) = original amplitudes => with ifft(fft(x) *fft(y)) you need
 % to devide by the length but only once
 %
-% it is not possible to just cut the correlation function with lags to get
+% it is NOT possible to just cut the correlation function with lags to get
 % a location dependent correlation function. The reason for that is that
 % if there is a lot of movement in space, the spherical harmonics contain
 % also information about fluctutations different from the location
-% considered. Therefore, the simulation time has to be shortended.
+% considered. Therefore, the simulation time has to be shortended to get a
+% loaction dependent correlation function.
 %
 % NOTE: There is no offset suppression implemented. This have to be made in
 % the postprocessing part.
@@ -32,15 +31,18 @@ function [sumCorrelationFunction] = ...
 [~,timeSteps] = size(sphericalHarmonic);
 zeroPaddingLength = 2^(nextpow2(timeSteps));
 
-fftSphericalHarmonic = fft(sphericalHarmonic,zeroPaddingLength,2);
+% This is faster than double(fft(...
+% double for higher precision in correlation functions
+fftSphericalHarmonic = fft(double(sphericalHarmonic),zeroPaddingLength,2);
 
-multiplicationFunction = real(fftSphericalHarmonic).^2 ...
-    + imag(fftSphericalHarmonic).^2;
+% This is faster than real^2 + img^2
+% due to double, this should not change any precision
+correlationFunction = ifft(abs(fftSphericalHarmonic).^2 ,[],2);
+
 clearvars fftSphericalHarmonic
 
-correlationFunction = ifft(multiplicationFunction,[],2,'symmetric');
-
-sumCorrelationFunction = sum(correlationFunction(:,1:numLags),1)/timeSteps;
+sumCorrelationFunction = sum(correlationFunction(:,1:timeSteps),1) ...
+    /timeSteps;
 
  end
 
