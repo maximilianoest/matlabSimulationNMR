edbc2c44fafae636e1a9c2c79db438f6c9987e65
maximilianoest <50007913+maximilianoest@users.noreply.github.com>
2022-10-14 14:58:48 +0200

    Evaluation single vs. double precision corr func

--- evaluation/SingleVsDoublePrecision/compareSingleAndDoublePrecision.m
+++ evaluation/SingleVsDoublePrecision/compareSingleAndDoublePrecision.m
@@ -19,10 +19,10 @@ nNCases.lipidWater = 'nearestNeighbours8000';
 nNCases.lipid = 'nearestNeighbours5500';
 allMatFilesInResultsDirectory = dir( ...
     sprintf('%s%s*.mat',resultsDir,filesep));
-figPosAndSize = [50 50 1400 700];
+figPosAndSize = [50 50 1000 700];
+configVariables = who;
 
 relevantResults = struct();
-
 %% load data and put it into relevantResults:
 for fileCounter = 1:length(allMatFilesInResultsDirectory)
     % create file path
@@ -50,32 +50,118 @@ for fileCounter = 1:length(allMatFilesInResultsDirectory)
     corrFuncSecondOrderDouble = squeeze( ...
         simulationResults.sumCorrelationFunctionSaverSecondOrder.( ...
         whichCase)(theta,phi,:));
-    
-    % fill a struct
-    if ~isfield(relevantResults,whichLipid)
-        relevantResults.(whichLipid).constituent = constituent;
+   
+    if strcmp(simulationResults.constituent,'lipid')
+        arrayPos = 1;
     else
-        relevantResults.(whichLipid)(end+1).constituent = constituent;
+        arrayPos = 2;
     end
-    
-    relevantResults.(whichLipid)(end).deltaTInS ...
-            = simulationResults.deltaTInS;
-    relevantResults.(whichLipid)(end).corrFuncFirstOrderSingle = ...
+    relevantResults.(whichLipid)(arrayPos).constituent = constituent;
+    relevantResults.(whichLipid)(arrayPos).deltaTInS ...
+        = simulationResults.deltaTInS;
+    relevantResults.(whichLipid)(arrayPos).corrFuncFirstOrderSingle = ...
         corrFuncFirstOrderSingle;
-    relevantResults.(whichLipid)(end).corrFuncSecondOrderSingle = ...
+    relevantResults.(whichLipid)(arrayPos).corrFuncSecondOrderSingle = ...
         corrFuncSecondOrderSingle;
-    relevantResults.(whichLipid)(end).corrFuncFirstOrderDouble = ...
+    relevantResults.(whichLipid)(arrayPos).corrFuncFirstOrderDouble = ...
         corrFuncFirstOrderDouble;
-    relevantResults.(whichLipid)(end).corrFuncSecondOrderDouble = ...
+    relevantResults.(whichLipid)(arrayPos).corrFuncSecondOrderDouble = ...
         corrFuncSecondOrderDouble;
-    relevantResults.(whichLipid)(end).simulationDurationInS = ...
+    relevantResults.(whichLipid)(arrayPos).simulationDurationInS = ...
         simulationResults.simulationDurationInS;
-    relevantResults.(whichLipid)(end).fibreAnglesTheta = ...
+    relevantResults.(whichLipid)(arrayPos).fibreAnglesTheta = ...
         simulationResults.fibreAnglesTheta;
-    relevantResults.(whichLipid)(end).fibreAnglesPhi = ...
+    relevantResults.(whichLipid)(arrayPos).fibreAnglesPhi = ...
         simulationResults.fibreAnglesPhi;
-    relevantResults.(whichLipid)(end).matlabSimulationDate = ...
+    relevantResults.(whichLipid)(arrayPos).matlabSimulationDate = ...
         simulationResults.matlabSimulationDate;
 end
-
+clearvars('-except',configVariables{:},'relevantResults');
 %% analyse data:
+corrFuncNames = ["corrFuncFirstOrder" "corrFuncSecondOrder"];
+
+for whichLipid = string(fieldnames(relevantResults)')
+    lipidData = relevantResults.(whichLipid);
+    fig = initializeFigure('posAndSize',figPosAndSize);
+    subplotCounter = 1;
+    for datasetNr = 1:length(lipidData)
+        constituentData = lipidData(datasetNr);
+        for corrFuncCaseNr = 1:length(corrFuncNames)
+            initializeSubplot(fig,2,2,subplotCounter);
+            switch subplotCounter
+                % [left bottom width height]
+                case 1
+                    left = 0.05;
+                    bottom = 0.55;
+                    
+                case 2
+                    left = 0.55;
+                    bottom = 0.55;
+                case 3
+                    left = 0.05;
+                    bottom = 0.055;
+                case 4
+                    left = 0.55;
+                    bottom = 0.055;
+            end
+            set(gca,'Position',[left bottom 0.43 0.42]);
+            subplotCounter = subplotCounter + 1;
+            
+            corrFuncSingle = constituentData.(corrFuncNames( ...
+                corrFuncCaseNr)+"Single");
+            corrFuncDouble = constituentData.(corrFuncNames( ...
+                corrFuncCaseNr)+"Double");    
+            difference = corrFuncDouble - corrFuncSingle;
+            timeAxis = 0:constituentData.deltaTInS:constituentData ...
+                .simulationDurationInS;
+            lastPart = 80000;
+            
+            timeAxis = timeAxis(end-lastPart:end);
+            plot(timeAxis,real(corrFuncDouble(end-lastPart:end)));
+            plot(timeAxis,imag(corrFuncDouble(end-lastPart:end)));
+            plot(timeAxis,real(corrFuncSingle(end-lastPart:end)),'--');
+            plot(timeAxis,imag(corrFuncSingle(end-lastPart:end)),'--');
+            plot(timeAxis,real(difference(end-lastPart:end)));
+            plot(timeAxis,imag(difference(end-lastPart:end)),'--');
+
+            axis([-inf inf -inf inf]);
+            
+            if corrFuncCaseNr == 1
+                ylabel(constituentData.constituent);
+            end
+            
+            if datasetNr == 2
+                if corrFuncCaseNr == 1
+                    xlabel('First order Corr. Func.');
+                else
+                    xlabel('Second order Corr. Func.');
+                end
+            end
+                
+            if datasetNr == 1 && corrFuncCaseNr == 1
+                lgd = legend( ...
+                    'Real part double','Imag. part double' ...
+                    ,'Real part single','Imag. part single' ...
+                    ,'Real part diff. dou. - sing.' ...
+                    ,'Imag. part diff. dou. - sing.');
+                lgd.Location = 'east';
+                title(sprintf('Lipid: %s, Cumsum last part %i steps, double vs. single' ...
+                    ,whichLipid,lastPart));
+            else
+                lgd = legend();
+                lgd.Location = 'east';
+                lgd.Visible = 'Off';
+            end
+        end
+    end
+    if saving
+        saveFigureTo(savingDirectory,whichLipid ...
+            ,constituentData.matlabSimulationDate ...
+            ,'ComparisonCorrFuncSingleDouble');
+    end
+    
+end
+
+
+
+
