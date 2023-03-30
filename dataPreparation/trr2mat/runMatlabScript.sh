d256c6f16babdff5eff070fd74da8e6be2355070
maximilianoest <50007913+maximilianoest@users.noreply.github.com>
2023-03-30 13:45:14 +0200

    Update runMatlabScript.sh

--- dataPreparation/trr2mat/runMatlabScript.sh
+++ dataPreparation/trr2mat/runMatlabScript.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-echo "Sleeping till GROMACS is ready"
-sleep 7h
-echo "Awakening to run Matlab script"
-echo "Running matlab script"
-matlab -nosplash -nodisplay -nodesktop -r 'try; createMatFileFromTrrFile; catch e; disp(e.message); disp(e.stack); save(sprintf("%s_Error",datestr(now,"yyyymmdd")),"e");  end; quit;' > Monolayer_wholeDatasetCreation.log
+matlab -nosplash -nodisplay -nodesktop -r 'try; createMatFileFromTerminateTrrFile; catch e; disp(e.message); disp(e.stack); save(sprintf("%s_Error",datestr(now,"yyyymmdd")),"e");  end; quit;' > Bilayer_distributionAnalysis.log
